3284f106288eb231b0ad3747c722439c777fb81f
wgy <wgyscsf@163.com>
2016-04-11 21:15:46 +0800

    新建servlet

---
+++ elife/src/com/elife/web/servlet/app/DemoServlet.java
@@ -0,0 +1,13 @@
+package com.elife.web.servlet.app;
+
+/**
+ * @author 高远</n>
+ * 编写时期  2016-4-11 下午9:15:28</n>
+ * TODO</n>
+ * 邮箱：wgyscsf@163.com</n>
+ * 博客  http://blog.csdn.net/wgyscsf</n>
+ * 
+ */
+public class DemoServlet {
+
+}
