e57d73dc4f7347bc2c7dce3ec44734baf19569aa
wgy <wgyscsf@163.com>
2016-04-11 21:39:43 +0800

    添加简单的用户注册

---
+++ elife/src/com/elife/service/impl/UserService.java
@@ -0,0 +1,22 @@
+package com.elife.service.impl;
+
+import com.elife.beans.Users;
+import com.elife.service.inter.IUserService;
+
+/**
+ * @author 高远</n>
+ * 编写时期  2016-4-11 下午9:38:13</n>
+ * TODO</n>
+ * 邮箱：wgyscsf@163.com</n>
+ * 博客  http://blog.csdn.net/wgyscsf</n>
+ * 
+ */
+public class UserService implements IUserService {
+
+	@Override
+	public boolean addUser(Users users) {
+
+		return false;
+	}
+
+}
